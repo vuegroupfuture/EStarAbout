13e953e687864a1d9424c2d8b5b7a120aa791491
pengweinan <18521717651@163.com>
2019-07-17 20:08:36 +0800

    项目

---
+++ pengweinan/src/components/AboutCell.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="about-cell">
+        <div id="left">{{content.leftLab}}</div>
+        <div id="right">{{content.rightLab}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutCell',
+    props: {
+        content: {
+          leftLab:String,
+          rightLab:String
+        }
+    }
+}
+</script>
+
+
+<style>
+#about-cell {
+    width: 100%;
+    background-color: white;
+    border: 1;
+    border-bottom-color: gray;
+}
+#left {
+    padding: 10px;
+    text-align: left;
+    width: 30%;
+    display: inline-block;
+}
+#right {
+    text-align: right;
+    width: 60%;
+    display: inline-block;
+}
+</style>
---
+++ pengweinan/src/views/About.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="about">
+    <img id="estarlogo" src="../assets/estarlogo.png" alt="estar logo">
+    <h5 id="estartitle">易星</h5>
+    <AboutCell id="about-cell" v-for="content in contents" :key="content.id" :content="content"/>
+  </div>
+</template>
+
+<script>
+import AboutCell from '@/components/AboutCell.vue'
+export default {
+  name: 'about',
+  components: {
+    AboutCell
+  },
+  data:function() {
+    return {
+      contents:[
+      {
+        leftLab: "包号",
+        rightLab: "000734"
+      },
+      {
+        leftLab: "即时行情",
+        rightLab: "120.136.160.83:55013"
+      },
+      {
+        leftLab: "历史行情",
+        rightLab:"212.21.23.23.23.43"
+      },
+      {
+        leftLab: "云交易地址",
+        rightLab: "43.4.45.544.344.3343"
+      }
+    ]
+    }
+    
+  }
+}
+</script>
+
+
+<style>
+#about {
+  width: 100%;
+  height: 100%;
+  background: gray;
+}
+#estarlogo { 
+  width: 20%;
+  height: 20%;
+}
+#estartitle {
+  font-size: 16px;
+}
+#about-cell {
+  width: 100%;
+  height: 40px;
+  border-top: 1px solid rgba(7, 17, 27, 0.1);
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+}
+</style>
