fea6e6aa277e288e2c0bc1052c3dda2f7e229bb1
pengweinan <18521717651@163.com>
2019-08-30 17:07:40 +0800

    轮播滑动

--- pengweinan/src/components/CarouselImage.vue
+++ pengweinan/src/components/CarouselImage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="slider">
-        <div class="window">
-            <div class="container" :style="containerStyle">
+        <div class="window" @mouseover="stop" @mouseleave="play">
+            <div class="container" :style="[containerStyle,baseContainerStyle]">
                 <div class="image">
                     <img class="img" :src="carouselImages[carouselImages.length-1].img" alt="">
                 </div>
@@ -12,10 +12,11 @@
                     <img class="img" :src="carouselImages[0].img" alt="">
                 </div>
             </div>
-            <div class="icon-left-arrow"></div>
-            <div class="icon-right-arrow"></div>
+            <div class="icon-left-arrow" @click="move(1)"></div>
+            <div class="icon-right-arrow" @click="move(-1)"></div>
             <div class="dots">
-                <div class="dot" v-for="(image, index) in carouselImages"></div>
+                <div class="dot" v-for="(image, index) in carouselImages" :key="index" 
+                :class="[(index == currentIndex) ? 'selected-dot' : '']" @click="clickMove(index)"></div>
             </div>
             
         </div>
@@ -23,6 +24,7 @@
 </template>
 
 <script>
+import { win32 } from 'path';
 
 export default {
     name: 'carouselImage',
@@ -34,24 +36,94 @@ export default {
         duration: {
             type: Number,
             default: function() {
-                return 4;
+                return 3;
             }
         },
     },
     data: function() {
         return {
-            currentIndex: 1,
-            distance: -414
+            currentIndex: 0,
+            unitDistance: 414,
+            distance: -414,
+            imageCount: this.carouselImages.length,
+            speed: 30,
+            transitionEnd: true
         }
     },
-    // methods: {
-        
-    // },
+    mounted: function() {
+        this.init()
+    },
     computed: {
         containerStyle() {
             return {
-                // transform: `translate3d(${this.distance}px, 0, 0)`
+                transform: `translate3d(${this.distance}px, 0, 0)`
             }
+        },
+        containerWidth: function() {
+            return this.unitDistance * (this.carouselImages.length + 2);
+        },
+        baseContainerStyle () {
+            return {
+                width: `${this.containerWidth}px`
+            }
+        },
+        animateDuration: function() {
+            return this.unitDistance / this.speed;
+        },
+        interval() {
+            return this.duration * 1000;
+        }
+        
+    },
+    methods: {
+        init () {
+            this.play()  
+        },
+        move(direct) {
+
+            if(!this.transitionEnd) return;
+            this.transitionEnd = false;
+            
+            this.currentIndex -= direct;
+            if(this.currentIndex < 0) this.currentIndex = 2;
+            if(this.currentIndex > 2) this.currentIndex = 0;
+
+            const des = this.distance + this.unitDistance * direct;
+            this.animate(des, this.speed, direct);
+        },
+        animate(des, speed, direct) {
+            if(this.temp) {
+                window.clearInterval(this.temp);
+                this.temp = null;
+            }
+            this.temp = window.setInterval(() => {
+                if((direct == -1 && des < this.distance) || (direct == 1 && des > this.distance)) {
+                    this.distance += speed * direct;
+                } else {
+                    this.transitionEnd = true;
+                    window.clearInterval(this.temp);
+                    this.distance = des;
+                    if(this.distance <= (-1 * this.containerWidth)) this.distance = -1 * this.unitDistance;
+                    if(this.distance >= 0) this.distance = this.unitDistance * (-1) * this.carouselImages.length;
+                }
+            },this.animateDuration)
+        },
+        clickMove(index) {
+            this.currentIndex = index;
+            this.distance = -(index+1) * this.unitDistance;
+        },
+        play() {
+            if (this.carouselTimer) {
+                window.clearInterval(this.carouselTimer)
+                this.carouselTimer = null
+            }
+            this.carouselTimer = window.setInterval(() => {
+                this.move(-1)
+            }, this.interval);
+        },
+        stop () {
+            window.clearInterval(this.carouselTimer)
+            window.carouselTimer = null
         }
     }
 }
